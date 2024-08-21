f3ebcdb7a27b141da0d7605380420727783198a2
Anirudh-x <aniruddharautofficial@gmail.com>
2024-08-21 21:56:54 +0530

    Update Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import homeImage from "../assets/Home(White).png"
 
 const Home = () => {
   return (
-    <div className="h-[100vh] w-full bg-black text-white flex items-center justify-center sm:justify-start">
+    <div className="h-[100vh] w-full bg-black text-white flex items-center justify-center">
 
-      <div className="flex flex-col items-center sm:items-start sm:ml-10">
-        <h1 className="text-[8rem] font-semibold">InfyTeck</h1>
-        <h3 className="">From Concept To Creation</h3>
+      <div className="flex flex-col items-center sm:ml-10">
+        <img src={homeImage} className="h-[30rem] w-[60rem]"/>
 
-        <div className="mt-[5rem] flex gap-4">
+        <div className="flex gap-4">
           <button className="bg-white border-white border-2 hover:bg-black hover:text-white text-black px-5 py-3 rounded-[40px]">
             Explore Now
           </button>
